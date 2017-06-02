01ae08b3bd3da6df8cb9b5ef76bd7f75fa192898
Alyssa Moody <alyssanicholemoody@gmail.com>
2017-06-02 12:26:29 -0700

    Add post to HomeModule.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -11,6 +11,11 @@ namespace WordCounter
       Get["/"] = _ => {
         return View["index.cshtml"];
       };
+      Post["/result"] = _ => {
+        RepeatCounter newCounter = new RepeatCounter(Request.Form["user-search-word"], Request.Form["user-text"]);
+        int count = newCounter.CountRepeats();
+        return View["result.cshtml", newCounter];
+      };
     }
   }
 }
