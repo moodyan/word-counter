0d98d7221f1a0ddf270792076a3ca97ac41f8ca3
Alyssa Moody <alyssanicholemoody@gmail.com>
2017-06-02 12:23:52 -0700

    Begin creating Nancy app. Add get to HomeModule. Add html to index. Create result.cshtml. Add jumbotron and some styling.

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,16 @@
+using Nancy;
+using System;
+using WordCounter.Objects;
+
+namespace WordCounter
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        return View["index.cshtml"];
+      };
+    }
+  }
+}
